052b7f48d740bfc542d3314ba10f5053edab7735
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2024-12-29 16:50:00 +0200

    fix timezone

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -3,14 +3,16 @@ const moment = require('moment'); // For working with dates and times
 const bot = require('../bot'); // Your Telegram bot instance
 const TrainingSchedule = require('../models/TrainingSchedule'); // Training schedule model
 
+const UKRAINE_TIMEZONE = 'Europe/Kiev';
+
 // Cron job to check for upcoming trainings and send reminders
 cron.schedule('1 * * * *', async () => {
     console.log('Checking for upcoming trainings to send reminders...');
     
     try {
         // Get the current time and the time one hour later
-        const now = moment();
-        const oneHourLater = moment().add(1, 'hours');
+        const now = moment().tz(UKRAINE_TIMEZONE);
+        const oneHourLater =now.clone().add(1, 'hours');
 
         // Find trainings that will start in the next hour
         const upcomingTrainings = await TrainingSchedule.find({
